c042cf8c2d19cf875c7e1a397e82ffa8d058547e
jan_pastucha <109542445+Jahus1loh@users.noreply.github.com>
2025-08-09 19:25:55 +0200

    final solution

--- solution.py
+++ solution.py
@@ -1,20 +1,11 @@
 import pandas as pd
 import re
 
+# Function to add new_column to the df based on the expression in role variable
 def add_virtual_column(df: pd.DataFrame, role: str, new_column: str):
-    if not contains_only_letters_and_underscores(new_column):
-        return pd.DataFrame()
-    
     role = get_column_names_and_operation_type(role)
     
-    for series_name, series in df.items():
-        if not contains_only_letters_and_underscores(series_name):
-            return pd.DataFrame()
-    
-    if  len(role) != 3 or not (
-        contains_only_letters_and_underscores(role[0]) and 
-        contains_only_letters_and_underscores(role[2]) and 
-        check_if_role_column_names_exist_in_df(role, df)):
+    if check_if_any_column_name_is_incorrect(df, role, new_column):
         return pd.DataFrame()
         
     if role[1] == "+":
@@ -27,44 +18,60 @@ def add_virtual_column(df: pd.DataFrame, role: str, new_column: str):
         return perform_substraction(df, role[0], role[2], new_column)
     
     return pd.DataFrame()
-    
-def check_if_role_column_names_exist_in_df(roles: list[str], df: pd.DataFrame):
-    try:
-        df[roles[0]]
-        df[roles[2]]
-        return True
-    except Exception as e:
-        print("There isn't column with this name in the dataframe")
-        return False
-    
 
+
+# Function to check if column names in the df, role and new_column have only letters and underscores
+def check_if_any_column_name_is_incorrect(df, role, new_column):
+    if len(role) != 3: return True # This will happen if column name in role contains -, + or *
+    if not contains_only_letters_and_underscores(role[0]): return True # This will happen when first column name has some incorrect characters
+    if not contains_only_letters_and_underscores(role[2]): return True # This will happen when second column name has some incorrect characters
+    if not check_if_column_names_in_df_are_correct(df): return True # This will happen when some column name in df has some incorrect characters
+    if not check_if_role_column_names_exist_in_df(role, df): return True # This will happen when role tries to use some column that doesn't exist in df
+    if not contains_only_letters_and_underscores(new_column): return True # This will happen when new column name has some incorrect characters
+    
+    return False
+    
+    
+# Function to check if column names specified in roles are present in df
+def check_if_role_column_names_exist_in_df(roles: list[str], df: pd.DataFrame):
+    return set([roles[0], roles[2]]).issubset(df.columns)
+        
+    
+# Function to check if every column name in the df has only letters and underscores
+def check_if_column_names_in_df_are_correct(df):
+    for series_name, series in df.items():
+        if not contains_only_letters_and_underscores(series_name):
+            return False
+        
+    return True
+
+# Function to perform addition of df columns
 def perform_addition(df: pd.DataFrame, first_column: str, second_column: str, new_column:str):
     df[new_column] = df[first_column] + df[second_column]
     return df
     
+    
+# Function to perform multiplication of df columns
 def perform_multiplication(df: pd.DataFrame, first_column: str, second_column: str, new_column:str):
     df[new_column] = df[first_column] * df[second_column]
     return df
 
+
+# Function to perform substraction of df columns
 def perform_substraction(df: pd.DataFrame, first_column: str, second_column: str, new_column:str):
     df[new_column] = df[first_column] - df[second_column]
     return df
 
-# Helper function to check if word contains only letters and underscores
+
+# Function to check if word contains only letters and underscores
 def contains_only_letters_and_underscores(word: str): 
-    return bool(re.fullmatch("[a-zA-z_]+", word))
+    return bool(re.fullmatch("[A-Za-z_]+", word))
 
 
-# Helper function to retrieve the column names and operation type from the role variable
+# Function to retrieve the column names and operation type from the role variable
 def get_column_names_and_operation_type(role: str):
-    column_names_and_operation_type = re.split(r'([+\*\-])', role)
-    column_names_and_operation_type = [x.replace(" ", "") for x in column_names_and_operation_type]
-    
-    #column_names_and_operation_type[1] = deterimne_operation_type(column_names_and_operation_type[1])
-        
+    column_names_and_operation_type = re.split(r'([+\*\-])', role) # Split the role string into a list on +, - or *, and include the operation sign
+    column_names_and_operation_type = [x.replace(" ", "") for x in column_names_and_operation_type] # Remove all spaces from the strings
+            
     return column_names_and_operation_type
-        
-    
-df = pd.DataFrame([[1, 1]] * 2, columns = ["label_one", "label_two"])
 
-print(add_virtual_column(df, "label_one + label_two", "label_three"))
